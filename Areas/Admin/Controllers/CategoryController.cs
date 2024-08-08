c445fc0762866151a4e194abfb77a43267b2a578
PBGaming11 <phanbaobeo789@gmail.com>
2024-08-08 10:11:42 +0700

    Add project files.

---
+++ Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteTMDT.Areas.Admin.Models.EF;
+using WebsiteTMDT.Data;
+
+namespace WebsiteTMDT.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var item = _db.Categories;
+            return View(item);
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(Category model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.CreateDate = DateTime.Now;
+                model.ModifierDate = DateTime.Now;
+                _db.Categories.Add(model);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Areas/Admin/Models/EF/Post.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebsiteTMDT.Areas.Admin.Models.EF
+{
+    [Table("tb_Post")]
+    public class Post : CommonAbstract
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+        public int CategoryId { get; set; }
+        public string? Description { get; set; }
+        public string? Detail { get; set; }
+        public string? Image { get; set; }
+        public string? SeoTitle { get; set; }
+        public string? SeoDescription { get; set; }
+        public string? SeoKeywords { get; set; }
+        public int Position { get; set; }
+        public virtual Category Category { get; set; }
+    }
+}
